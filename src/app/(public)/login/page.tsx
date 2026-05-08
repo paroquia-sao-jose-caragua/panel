5908523ec1fe9ae7e0b7e11188f7ecafb6a12e78
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-05-08 10:15:19 -0300

    style: enhance login page layout and styling for improved user experience

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -4,9 +4,9 @@ import { Form } from './form';
 
 export default function Login() {
   return (
-    <main className="min-h-screen flex flex-row items-center justify-center">
-      <div className='hidden lg:block h-screen w-1/2 bg-cover bg-center bg-[url("/login/cover.png")]' />
-      <div className="flex flex-col gap-6 row-start-2 items-center justify-center max-w-100 w-full mx-auto px-4 py-12">
+    <main className="relative min-h-screen flex flex-row items-center justify-center bg-brand-0">
+      <div className='absolute lg:relative z-0 block h-screen lg:w-1/2 w-full bg-cover bg-center bg-[url("/login/cover.png")]' />
+      <div className="z-10 lg:z-0 flex flex-col gap-6 row-start-2 items-center justify-center max-w-100 w-full lg:mx-auto bg-white rounded-2xl shadow-sm border border-gray-100 px-6 py-6 mx-4">
         <div className="flex flex-col items-center justify-center border-b border-divider pb-6 w-full">
           <Image
             src="/avatar.svg"
@@ -18,7 +18,9 @@ export default function Login() {
           <h1 className="text-2xl font-medium text-brand-900 mt-4">
             Acessar Painel
           </h1>
-          <Describe>Jesus, Maria e José, a nossa família vossa é!</Describe>
+          <Describe className="text-center">
+            Jesus, Maria e José, a nossa família vossa é!
+          </Describe>
         </div>
         <Form />
       </div>
