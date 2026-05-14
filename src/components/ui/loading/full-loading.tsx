37084830379d75cf8b8b8b377d9a9f26c988f1fb
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-05-14 08:30:35 -0300

    feat: enhance AuthGuardProvider to include loading state and pass children prop refactor: standardize quotes and improve logout route logic

--- src/components/ui/loading/full-loading.tsx
+++ src/components/ui/loading/full-loading.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from './spinner';
 
 export const FullLoading = () => (
-  <div className="absolute h-full w-full top-0 right-0 flex flex-col items-center justify-center gap-8 bg-brand-0">
+  <div className="absolute z-50 h-full w-full top-0 right-0 flex flex-col items-center justify-center gap-8 bg-brand-0">
     <Spinner />
     <span className="text-lg font-medium text-brand-800">Carregando...</span>
   </div>
